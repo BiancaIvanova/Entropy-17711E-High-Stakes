d2405af3312d4e1c1ffae6944d7c45e60441b0c3
BiancaIvanova <bianca.ivanova.1.0@gmail.com>
2025-02-17 23:25:34 +0000

    Arm control pid done

--- src/other/arm-controller.cpp
+++ src/other/arm-controller.cpp
@@ -11,31 +11,40 @@ class ArmController {
     double maxVelocity;
     static const int TIME_STEP = 20;
     pros::Rotation rotationSensor;
-    pros::Motor armMotor;
+    pros::MotorGroup armMotors;  // MotorGroup for multiple motors
 
 public:
-    ArmController(double kP, double kI, double kD, double minPosition, double maxPosition, double maxVelocity, pros::Rotation rotationSensor, pros::Motor armMotor)
+    ArmController(double kP, double kI, double kD, double minPosition, double maxPosition, double maxVelocity, pros::Rotation rotationSensor, pros::MotorGroup armMotors)
         : pid(kP, kI, kD),
           minPosition(minPosition),
           maxPosition(maxPosition),
           maxVelocity(maxVelocity),
           rotationSensor(rotationSensor),
-          armMotor(armMotor) {}
+          armMotors(armMotors) {}
 
     void moveToPosition(double position) {
-        targetPosition = fmin(fmax(position, minPosition), maxPosition);
-        double const VELOCITY_THRESHOLD = 5;
-        double tolerance = 1.0;
+        targetPosition = fmin(fmax(position, minPosition), maxPosition);  // Clamp target position
+        double const VELOCITY_THRESHOLD = 5;  // Stop moving when velocity is low enough
+        double tolerance = 1.0;  // Position tolerance in degrees
+    
+        // Continue adjusting until the arm is within the tolerance of the target
+        while (fabs(currentPosition - targetPosition) > tolerance || 
+               fabs(armMotors.get_actual_velocity()) > VELOCITY_THRESHOLD) {
+            currentPosition = rotationSensor.get_position();  // Update current position
+            double error = targetPosition - currentPosition;  // Calculate the position error
 
-        while (armMotor.get_actual_velocity() > VELOCITY_THRESHOLD) {
-            currentPosition = rotationSensor.get_position();
-            double error = targetPosition - currentPosition;
-            double velocity = pid.calculate(error, TIME_STEP);
-            velocity = fmin(fmax(velocity, -maxVelocity), maxVelocity);
-            armMotor.move_velocity(velocity);
+            // Calculate velocity using PID controller
+            double velocity = pid.update(error, TIME_STEP);
+            velocity = fmin(fmax(velocity, -maxVelocity), maxVelocity);  // Clamp velocity
+
+            // Move the motor group with the calculated velocity
+            armMotors.move_velocity(velocity);
+
+            // Wait for the next time step
             pros::delay(TIME_STEP);
         }
 
-        armMotor.move_velocity(0);
+        // Stop the motor group when the target is reached
+        armMotors.move_velocity(0);
     }
 };
