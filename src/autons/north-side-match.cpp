85f9e721fd71e836a8492801e8626f3167efeda3
BiancaIvanova <bianca.ivanova.1.0@gmail.com>
2025-02-24 23:12:50 +0000

    Copied over implementation for blue awp

--- src/autons/north-side-match.cpp
+++ src/autons/north-side-match.cpp
@@ -63,3 +63,61 @@ void north_side_red()
     chassis.turnToPoint(-28, 7, 500);
     chassis.moveToPoint(-28, 7, 2000);
 }
+
+
+void north_side_blue()
+{
+    chassis.setPose(53, 10, 120);
+    arm.resetPosition(ArmPosition::LOAD);
+
+    // Place ring on alliance stake
+    chassis.moveToPoint(60.5, 6, 400);
+    arm.moveToPosition(ArmPosition::ALLIANCE_STAKE, true, 0.35);
+    pros::delay(750);
+
+    // Move back and grab right stake
+    mobile_stake_clamp.set_value(true);
+    chassis.moveToPoint(34, 21, 750, {.forwards=false}, false);
+    arm.moveToPosition(ArmPosition::DOWN);
+    chassis.moveToPoint(24, 26, 500, {.forwards=false, .maxSpeed = 50}, false); // slower
+    pros::delay(250);
+    mobile_stake_clamp.set_value(false);
+    pros::delay(250);
+    
+    // Turn and grab first ring
+    intake_controlled(600);
+    chassis.turnToPoint(29, 46, 750);
+    chassis.moveToPoint(29, 46, 1000);
+
+    // Grab second ring
+    chassis.turnToPoint(20, 51, 750);
+    chassis.moveToPoint(20, 51, 900);
+    chassis.moveToPoint(23, 48, 750, {.forwards=false}, false); // back off
+
+    // Move to other side of field
+    chassis.turnToPoint(49, 18, 750);
+    chassis.moveToPoint(-9, 18, 1400);
+    intake_controlled(0);
+    mobile_stake_clamp.set_value(true);
+    
+    // Grab the two ring stack
+    chassis.turnToPoint(46, -11, 750, {.maxSpeed=85}, false);
+    intake_controlled(600);
+    chassis.moveToPoint(46, 0, 1200, {});
+    chassis.moveToPoint(46, -11, 750);
+    pros::delay(975);
+    intake_controlled(0);
+
+    // Grab the other mogo
+    chassis.turnToPoint(30, -20, 750, {.forwards=false});
+    chassis.moveToPoint(30, -20, 750, {.forwards=false}, false);
+    chassis.moveToPoint(23, -23, 1000, {.forwards=false, .maxSpeed=20}, false);
+    pros::delay(250);
+    mobile_stake_clamp.set_value(false);
+    pros::delay(250);
+    intake_controlled(600);
+
+    // Touch the ladder
+    chassis.turnToPoint(28, 7, 500);
+    chassis.moveToPoint(28, 7, 2000);
+}
